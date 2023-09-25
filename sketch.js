1b58d110bda14a3b482514795b14337e973676c2
muhmuh987 <em.reynolds28@outlook.com>
2023-09-25 11:10:57 -0600

    add

--- sketch.js
+++ sketch.js
@@ -1,15 +1,48 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// mouse distance
+//mouse ahfusfasjf
+//I want to die
+let nodeSize = 15;
+let nodeColors = [];
+let colorIndex = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  initcolours();
+  segmentDistance();
+  textAlign(CENTER);
 }
 
 function draw() {
   background(220);
+  eragatr5gui();
+}
+function eragatr5gui(){
+  stroke(nodeColors[colorIndex]);
+  fill(nodeColors[colorIndex]);
+  circle(width/2, height/2, 20);
+  circle(mouseX, mouseY, 20);
+  line (width/2, height/2,mouseX,mouseY);
+  let d = segmentDistance(mouseX,width/2,mouseY,height/2);
+  textSize(20);
+  text(round(d,1), width/2, height * 0.4);
+}
+function initcolours(){
+  nodeColors.push(color("red"));
+  nodeColors.push(color(200,100,0));
+  nodeColors.push(color('blue'));
+}
+function mouseWheel(event){
+  print(event.delta);
+  if (event.delta > 0){
+    colorIndex += 1;
+    if (colorIndex > nodeColors.length -1){
+      colorIndex = 0;
+    }
+  }
+}
+function segmentDistance(x1,x2,y1,y2){
+  let a = Math.abs(x1 - x2);
+  let b = Math.abs(y1 - y2);
+  let c = Math.sqrt(a*a + b*b);
+  return c;
 }
